74445d92a7f45bfe35270ea78321a9d8b8320461
weilingxie <xietanzhen1@yeah.net>
2018-10-20 21:02:53 +1300

    YinYang animation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Config from '../Config.json';
-import BBC_Logo from '../BBC_News.svg';
+//import BBC_Logo from '../BBC_News.svg';
+import BBC_Logo from '../YinYang.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class HeaderComponent extends React.Component {
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,10 +6,6 @@ import LoginPage from '../components/LoginPage';
 import RegisterPage from '../components/RegisterPage';
 import NewsDetailPage from '../components/NewsDetailPage';
 import HomePage1 from '../components/HomePage1';
-import {
-    CSSTransition,
-    TransitionGroup,
-  } from 'react-transition-group';
 
 class AppRouter extends React.Component {
     constructor(props) {
@@ -31,23 +27,14 @@ class AppRouter extends React.Component {
     render() {
         return (
             <div>
-                <HeaderComponent />
                 <Route render={({ location })=>(
-                    <TransitionGroup>
-                        <CSSTransition
-                         key={location.key}
-                         timeout={450}
-                         classNames="fade"
-                        >
-                            <Switch location={location}>
-                                <Route path="/" render={(routeProps) => (<HomePage {...routeProps} />)} exact={true} />
-                                <Route path="/home1" render={(routeProps) => (<HomePage1 />)} exact={true} />                                
-                                <Route path="/login" render={(routeProps) => (<LoginPage {...routeProps} />)} exact={true} />
-                                <Route path="/register" render={(routeProps) => (<RegisterPage {...routeProps} />)} exact={true} />
-                                <Route path="/newsdetail/:news_id" render={(routeProps) => (<NewsDetailPage {...routeProps} />)} exact={true} />
-                            </Switch>
-                        </CSSTransition>
-                    </TransitionGroup>
+                    <Switch location={location}>
+                        <Route path="/" render={(routeProps) => (<HomePage {...routeProps} />)} exact={true} />
+                        <Route path="/home1" render={(routeProps) => (<HomePage1 />)} exact={true} />                                
+                        <Route path="/login" render={(routeProps) => (<LoginPage {...routeProps} />)} exact={true} />
+                        <Route path="/register" render={(routeProps) => (<RegisterPage {...routeProps} />)} exact={true} />
+                        <Route path="/newsdetail/:news_id" render={(routeProps) => (<NewsDetailPage {...routeProps} />)} exact={true} />
+                    </Switch>
                 )} />
                 
             </div>
