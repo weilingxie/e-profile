fe89dcd2954f56ea432cea423b4e5430f1343a20
weilingxie <xietanzhen1@yeah.net>
2018-11-17 12:29:29 +1300

    tune homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Config from '../Config.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -11,35 +11,29 @@ class Header extends React.Component {
 
     render() {
         return (
-                <div className="navbar-container">
-                        <Link to="/" className="navbar-brand">
-                            <p>Weiling Xie</p>
-                        </Link>
-                        <ul className="navbar-buttons">
-                            <li className="active">
-                                <Link to="/" className="navbar-home">
-                                    Home
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/aboutme" className="navbar-aboutme">
-                                    About Me
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/contact" className="navbar-contact">
-                                    Contact
-                                </Link>
-                            </li>
-                        </ul>                                                
-                        <div className="navbar-socialmedia">
-                            <a className="navbar-fackbook" href={Config.FACKBOOK_URL}>
-                                <FontAwesomeIcon icon={['fab','facebook']} size='3x'/>
-                            </a>
-                            <a className="navbar-linkedin" href={Config.LINKEDIN_URL}>
-                                <FontAwesomeIcon icon={['fab','linkedin']} size='3x'/>   
-                            </a>
-                        </div>
+                <div className="navbar">
+                    <Link to="/" className="navbar-brand">
+                        <p>Weiling Xie</p>
+                    </Link>
+                    <div className="navbar-buttons">
+                        <NavLink to="/" exact className="navbar-buttons-btn navbar-button-home" activeClassName="navbar-buttons-btn-active">
+                            Home
+                        </NavLink>
+                        <NavLink to="/aboutme" className="navbar-buttons-btn navbar-button-aboutme" activeClassName="navbar-buttons-btn-active">
+                            About Me
+                        </NavLink>
+                        <NavLink to="/contact" className="navbar-buttons-btn navbar-button-contact" activeClassName="navbar-buttons-btn-active">
+                            Contact
+                        </NavLink>
+                    </div>                                                
+                    <div className="navbar-socialmedia">
+                        <a className="navbar-socialmedia-item" href={Config.FACKBOOK_URL}>
+                            <FontAwesomeIcon icon={['fab','facebook']} size='2x'/>
+                        </a>
+                        <a className="navbar-socialmedia-item" href={Config.LINKEDIN_URL}>
+                            <FontAwesomeIcon icon={['fab','linkedin']} size='2x'/>   
+                        </a>
+                    </div>
                 </div>
             );
     }
